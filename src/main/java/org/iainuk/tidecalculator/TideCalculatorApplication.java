d95c519bff64a8a8c02114f229e616e4a2225af8
Iainuk86 <iainuk1986@gmail.com>
2021-02-25 22:45:45 +1100

    First commit

---
+++ src/main/java/org/iainuk/tidecalculator/TideCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.iainuk.tidecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TideCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TideCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iainuk/tidecalculator/TideCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iainuk.tidecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TideCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
